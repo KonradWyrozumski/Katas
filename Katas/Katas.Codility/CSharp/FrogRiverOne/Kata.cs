f918cf294db1bb8fb923acc5f0f9f8c9d1d82fb8
konradwyr <konrad.wyrozumski@gmail.com>
2015-08-08 16:15:00 +0200

    Added FrogRiverOne kata.

---
+++ Katas/Katas.Codility/CSharp/FrogRiverOne/Kata.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace Katas.Codility.CSharp.FrogRiverOne
+{
+    public class Kata
+    {
+        public int solution(int X, int[] A)
+        {
+            bool[] stepPassed = new bool[X + 1];
+            for (int i = 0; i < A.Count(); i++)
+            {
+                if (!stepPassed[A[i]])
+                {
+                    stepPassed[A[i]] = true;
+                    X--;
+                }
+                if (X == 0)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ Katas/Katas.Codility/CSharp/FrogRiverOne/KataTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+namespace Katas.Codility.CSharp.FrogRiverOne
+{
+    [TestFixture]
+    public class KataTests
+    {
+        [Test]
+        [TestCase(5, new[] { 1, 3, 1, 4, 2, 3, 5, 4 }, 6)]
+        [TestCase(5, new[] { 5, 1, 2, 3, 4 }, 4)]
+        [TestCase(6, new[] { 1, 2, 3, 4 }, -1)]
+        public void Solution_ForInput_ShouldReturnCorrectResult(int position, int[] inputArray, int result)
+        {
+            // Arrange
+            var kata = new Kata();
+
+            // Act
+            var solutionResult = kata.solution(position, inputArray);
+
+            // Assert
+            Assert.AreEqual(result, solutionResult);
+        }
+    }
+}
